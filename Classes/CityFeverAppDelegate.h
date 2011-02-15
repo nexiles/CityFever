8fa20cabfc6f5dfaa28f66cf23bf43a13f6a3d73
Stefan Eletzhofer <se@nexiles.de>
2011-02-15 08:45:29 +0100

    initial xcode template

---
+++ Classes/CityFeverAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  CityFeverAppDelegate.h
+//  CityFever
+//
+//  Created by Stephan Eletzhofer on 15.02.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityFeverAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
---
+++ Classes/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  RootViewController.h
+//  CityFever
+//
+//  Created by Stephan Eletzhofer on 15.02.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
