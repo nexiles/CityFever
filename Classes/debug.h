7afe5e8b910d59e188a4ad5dad32e4b3c6bb455d
Stefan Eletzhofer <se@nexiles.de>
2011-02-16 11:38:52 +0100

    dont log in release builds.

--- Classes/debug.h
+++ Classes/debug.h
@@ -7,9 +7,13 @@
  *
  */
 
-#define DBGS   NSLog(@"%s start", __FUNCTION__)
-#define DBG(x) NSLog(@"%s %s=%@", __FUNCTION__, #x, x)
-
+#ifdef DEBUG
+# define DBGS   NSLog(@"%s start", __FUNCTION__)
+# define DBG(x) NSLog(@"%s %s=%@", __FUNCTION__, #x, x)
+#else
+# define DBGS do {} while(0)
+# define DBG(x) do {} while(0)
+#endif
 #define RELEASE(x) [x release]; x = nil
 
 
